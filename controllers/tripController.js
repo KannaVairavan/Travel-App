9c898b394935d73021ebda2c06dc16c634353ffe
Kannathal Vairavan <kannamanickam@gmail.com>
2021-05-24 07:37:44 -0500

    Added user, trip model and modified controller, api-index routes accordingly

--- controllers/tripController.js
+++ controllers/tripController.js
@@ -1,34 +1,34 @@
 const db = require("../models");
 
-// Defining methods for the booksController
+// Defining methods for the tripController
 module.exports = {
   findAll: function(req, res) {
-    db.Book
+    db.Trip
       .find(req.query)
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
-    db.Book
+    db.Trip
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    db.Book
+    db.Trip
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.Book
+    db.Trip
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    db.Book
+    db.Trip
       .findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
--- models/index.js
+++ models/index.js
@@ -1,3 +1,5 @@
 module.exports = {
-  Book: require("./book")
+  
+  User: require("./user"),
+  Trip: require("./trip")
 };

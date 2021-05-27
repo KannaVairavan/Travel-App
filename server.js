45bde06ff252a228d9fdf0afcc1daa19054596bd
Ashley Lee <ashleyhayjinlee@gmail.com>
2021-05-26 22:03:25 -0500

    remove module

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3000;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));

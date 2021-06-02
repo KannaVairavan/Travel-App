cf827b577053082ec87e3312e4c137cc9efee135
ssolis1337 <71684140+ssolis86@users.noreply.github.com>
2021-06-01 21:19:37 -0500

    rendered generic map

---
+++ client/src/components/Map/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const GoogleMap = () => {
+ 
+    return (
+        <div>
+            <iframe
+                width="600"
+                height="450"
+                loading="lazy"
+                src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDWZTyEpdkP5oPR_6BPIjgv9IPXNzybgMc
+                    &q=Space+Needle,Seattle+WA">
+            </iframe>
+        </div>
+    )
+};
+
+export default GoogleMap;

d267bbe6455be07abd7345cfbf541a8f526cb5fb
Kannathal Vairavan <kannamanickam@gmail.com>
2021-06-06 17:42:59 -0500

    Modified wishlist model

--- models/wishlist.js
+++ models/wishlist.js
@@ -3,6 +3,10 @@ const Schema = mongoose.Schema;
 
 const WishListSchema  = new Schema({
     wish :{ type: String, required: true },
+    location:{ type: String, required: true },
+    coords_Lat:{ type: Number, required: true},
+    coords_Long:{ type: Number, required: true },
+
   user: [
     {
       type: Schema.Types.ObjectId,

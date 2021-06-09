df206e86f7ff71f686c7e81e77a34e34222d1f53
ssolis1337 <71684140+ssolis86@users.noreply.github.com>
2021-06-08 23:21:49 -0500

    response from GooglePlaces nearbySearch

--- controllers/nearbySearchController.js
+++ controllers/nearbySearchController.js
@@ -1,8 +1,13 @@
 
-
+const axios =require('axios');
 
 module.exports = {
   nearbySearch: function(req, res) {
-        res.send("this is a test")
+
+    const response = axios.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=AIzaSyDWZTyEpdkP5oPR_6BPIjgv9IPXNzybgMc")
+    .then(res => {
+      console.log(res.data.results);
+    })
+    res.send(response);
   }
 };

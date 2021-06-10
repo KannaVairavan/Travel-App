88a4d3cec54bf785b51df1852414c692e288cb0c
Joe Malone <joe.d.malone@gmail.com>
2021-06-10 13:07:44 -0500

    Added items of interest, wiki link, covid link, incorporated material ui-icons instead of flat hyperlink text, added 'favorited Icons'

--- client/src/components/Location/index.js
+++ client/src/components/Location/index.js
@@ -1,56 +1,60 @@
 import React, { useState, useEffect } from "react";
 import { Row, Col, Container } from "../Grid";
-import {input, FormBtn} from '../FormBtn'
+import { input, FormBtn } from "../FormBtn";
 import { SearchGoatByID } from "../../utils/SearchLocation";
+import DirectionsBusTwoToneIcon from "@material-ui/icons/DirectionsBusTwoTone";
+import LocalHospitalIcon from "@material-ui/icons/LocalHospital";
+import LocalLibraryIcon from "@material-ui/icons/LocalLibrary";
+import FavoriteButton from "../FavoriteButton"
 import "./style.css";
 import API from "../../utils/API";
 
-function LocationCard({ data },props) {
+function LocationCard({ data }, props) {
   const [results, setResults] = useState([]);
-  
-  const [detailedResults, setDetails] = useState([]);
-
-  
 
   useEffect(() => {
-    console.log("LocationCard", data)
-    setResults(data );
+    console.log("LocationCard", data);
+    setResults(data);
     if (data.length) {
-      // console.log(data[0].details.data.attributes.foursquare_url)
-      console.log("Passed Data: ", data );
-    } console.log('no data')
-
+      // console.log(data[0].details.data.attributes.covid["Travis County"].url);
+      console.log("Passed Data: ", data);
+    }
+    console.log("no data");
   }, [data]);
 
-
   const preciseRating = (number) => {
-    return(
-      number.toPrecision(3)
-    )
-  }
+    return number.toPrecision(3);
+  };
 
-  
-
-  
-   const handleFormSubmit=(event, index)=>{
+  const handleFormSubmit = (event, index) => {
     console.log(index);
-      event.preventDefault();
-        
-     const locationValues = results[index]
-     console.log("location data",locationValues);
-        API.savewishlist({
-            location_id:locationValues.city_id,
-            location:locationValues.cityName,
-            coords_Lat:locationValues.coords.lat,
-            coords_Lon:locationValues.coords.lon
-        })
-       .then ((res)=>{
-          console.log(res)
-       })
-       .catch(err => console.log(err));
-   
+    event.preventDefault();
 
-}
+    const locationValues = results[index];
+    console.log("location data", locationValues);
+    API.savewishlist({
+      location_id: locationValues.city_id,
+      location: locationValues.cityName,
+      coords_Lat: locationValues.coords.lat,
+      coords_Lon: locationValues.coords.lon,
+    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  // const enumerateCovid = (object) => {
+  //   for (const [key, value] of Object.entries(locations.details.data.attributes.covid)
+  // }
+  const enumerateCovid = (object) => {
+    const targetURLs = [];
+    for (const [key, value] of Object.entries(object)) {
+      targetURLs.push(`${key}: ${value.url}`);
+    }
+    console.log(targetURLs);
+    return targetURLs[0].split(": ")[1];
+  };
 
   return (
     <Container className={"-results-card-body "}>
@@ -66,39 +70,82 @@ function LocationCard({ data },props) {
           <Col key={index} size="md-2">
             <div className="card" style={{ width: "18rem", margin: "10px" }}>
               <img
-                src={locations.image_info.img_src== null? "https://via.placeholder.com/150.png": locations.image_info.img_src}
+                src={
+                  locations.image_info.img_src == null
+                    ? "https://via.placeholder.com/150.png"
+                    : locations.image_info.img_src
+                }
                 className="card-img-top"
                 alt="..."
               />
+              <FavoriteButton></FavoriteButton>
               <div className="card-body">
-                <h5 className="card-title" >{locations.cityName}</h5>
-
-                <p className="card-text">
-                {/* {locations.details.data.attributes.average_rating == null} ? "na" : "Tourist Rating: " {locations.details.data.attributes.average_rating} */}
-                </p>
+                <Row className="-card-header-row">
+                  <Col size={"md-8"}>
+                    <h5 className="card-title">{locations.cityName}</h5>
+                  </Col>
+                  <Col size={"md-4 rating-col"}>
+                    <h5 className="card-text city-rating-title">Rating </h5>
+                    <h5 className="card-text city-rating">
+                      {preciseRating(
+                        locations.details.data.attributes.average_rating
+                      )}
+                    </h5>
+                  </Col>
+                </Row>
               </div>
               <ul className="list-group list-group-flush">
-                <li className="list-group-item cit-coordinates">
+                {/* <li className="list-group-item cit-coordinates">
                   Lat: {locations.coords.lat} <br />
                   Long: {locations.coords.lon}
                 </li>
                 <li className="list-group-item">A second item</li>
-                <li className="list-group-item">A third item</li>
+                <li className="list-group-item">A third item</li> */}
               </ul>
               <div className="card-body">
-                <a href="#" className="card-link">
-                  Card link
-                </a>
-                <a href="#" className="card-link">
-                  Another link
-                </a>
-                
-                <FormBtn  onClick={(event)=>handleFormSubmit(event, index)} >Add to fav</FormBtn>
-                
+                <Row className="-card-header-row">
+                  <Col size={"md-6 card-links"}>
+                      <a
+                        href={
+                          locations.details.data.attributes.getyourguide_url
+                        }
+                        target="_blank"
+                        className={"guide-icon"}
+                      >
+                        <DirectionsBusTwoToneIcon className={"guide-icon"} />
+                      </a>
+                    <a
+                      href={locations.details.data.attributes.wikipedia_url}
+                      target="_blank"
+                      className={"guide-icon"}
+                    >
+                      <LocalLibraryIcon />
+                    </a>
+                    <a
+                      href={enumerateCovid(
+                        locations.details.data.attributes.covid
+                      )}
+                      target="_blank"
+                      className={"guide-icon"}
+                    >
+                      <LocalHospitalIcon color="secondary" />
+                    </a>
+                  </Col>
+                  <Col size={"md-6"}>
+
+
+                    <FormBtn
+                      onClick={(event) => handleFormSubmit(event, index)}
+                    >
+                      Add to fav
+                    </FormBtn>
+
+
+                  </Col>
+                </Row>
               </div>
             </div>
           </Col>
-          
         ))}
       </Row>
     </Container>
